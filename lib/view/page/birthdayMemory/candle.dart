3a2f9e80a94648d5049c95069196ea95705e7529
sotatt <pg6eat5f@i.softbank.jp>
2025-12-15 15:48:39 +0900

    create candle.dart

---
+++ lib/view/page/birthdayMemory/candle.dart
@@ -0,0 +1,89 @@
+// キャンドル画面
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:candlecatch/constants/colors.dart';
+
+class candle extends StatelessWidget {
+  final String year;
+  final String imagePath;
+
+  const candle({super.key, required this.year, required this.imagePath});
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/candleBackScreen.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            children: [
+              _BestShot(year: year, imagePath: imagePath),
+              //TODO: キャンドル追加
+              //TODO: ナビバー追加
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _BestShot extends StatelessWidget {
+  final String year;
+  final String imagePath;
+
+  const _BestShot({super.key, required this.year, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        // 誕生日表示
+        Text(
+          year,
+          style: const TextStyle(
+            fontFamily: "Corporate Logo Rounded Bold",
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+            color: AppColors.textWhite,
+          ),
+        ),
+        SizedBox(height: height * 0.05),
+        Hero(
+          tag: "$imagePath-$year",
+          child: Container(
+            width: width * 0.57,
+            height: height * 0.35,
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.3),
+                  blurRadius: 15,
+                  offset: const Offset(0, 10),
+                ),
+              ],
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(4),
+              child: Image.asset(imagePath, fit: BoxFit.cover),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
